675a578d70cb8505bd2829b27d63044604e8cb2d
Manami Ueda <manamiueda1@gmail.com>
2019-09-26 17:44:08 -0400

    added form responses to database

--- app.js
+++ app.js
@@ -4,9 +4,26 @@ const main = require("./views/main");
 const models = require('./models');
 
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+
+app.use('/wiki', require('./routes/wiki'))
+app.use('/user', require('./routes/user'))
+
+app.get('/', (req, res) => {
+    res.redirect('/wiki')
+});
+
+//   User.sync()
+//   Page.sync()
+
+
+
+
 const init = async () => {
-    await models.User.sync({force: true})
-    await models.Page.sync({force: true})
+    // await models..sync({force: true})
+    await models.db.sync({force: true})
+console.log('database succeeded!>>>>>>>>>>>>>>>>>>>')
 
     let PORT = 3001;
     app.listen(PORT, () => {
@@ -14,17 +31,4 @@ const init = async () => {
     })
 }
 init();
-
-app.use('/wiki', require('./routes/wiki'))
-app.use('/user', require('./routes/user'))
-
-//   User.sync()
-//   Page.sync()
-
-app.listen(3000, () => {
-    console.log('listening to port')
-})
-
-
-
 
--- routes/wiki.js
+++ routes/wiki.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router()
-
-const models = require('../models');
+const addPage = require('../views/addPage');
+const models = require('../models/index');
 
 router.get('/', async(req, res, next)=>{
     try{
+        // res.send('model page', await models.Page.findAll().map())
         res.send('got to GET /wiki/')
     } catch (err){
        console.log(err)
@@ -13,7 +14,26 @@ router.get('/', async(req, res, next)=>{
 
 router.post('/', async(req, res, next)=>{
     try{
-        res.send('got to POST /wiki/')
+        const name = req.body.name
+        const email = req.body.email
+        const title = req.body.title
+
+        function getSlug (title){
+            let regex = /[^0-9a-zA-Z]/gi
+            return title.replace(regex, '_');
+        }
+        let slug = getSlug(title)
+        console.log('slug', slug)
+
+        const page = new models.Page({
+            title: req.body.title,
+            content: req.body.content,
+            slug: slug,
+            status: req.body.status
+          });
+          await page.save();
+          res.redirect('/');
+        
     } catch (err){
        console.log(err)
     }
@@ -21,7 +41,7 @@ router.post('/', async(req, res, next)=>{
 
 router.get('/add', async(req, res, next)=>{
     try{
-        res.send('got to GET /wiki/add')
+        res.send(addPage())
     } catch (err){
        console.log(err)
     }
